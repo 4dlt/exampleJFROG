2dc066aecbd236b436b414a1902d2b5bf2a220da
Álvaro de la Torre <78616651+4dlt@users.noreply.github.com>
2023-11-03 01:03:30 +0100

    added vuln summary script

---
+++ summarize_vulnerabilities.py
@@ -0,0 +1,40 @@
+
+import json
+
+def summarize_vulnerabilities(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    
+    # Initialize a dictionary to count vulnerabilities by severity
+    vulnerabilities_summary = {
+        'UNKNOWN': 0,
+        'LOW': 0,
+        'MEDIUM': 0,
+        'HIGH': 0,
+        'CRITICAL': 0
+    }
+
+    # Recursive function to find the vulnerabilities in the data
+    def find_vulnerabilities(data):
+        if isinstance(data, dict):
+            for key, value in data.items():
+                if key == 'Vulnerabilities':
+                    for vulnerability in value:
+                        severity = vulnerability.get('Severity', 'UNKNOWN').upper()
+                        vulnerabilities_summary[severity] += 1
+                else:
+                    find_vulnerabilities(value)
+        elif isinstance(data, list):
+            for item in data:
+                find_vulnerabilities(item)
+
+    # Find the vulnerabilities and count them by severity
+    find_vulnerabilities(data)
+
+    # Print the summary
+    for severity, count in vulnerabilities_summary.items():
+        print(f'{severity}: {count}')
+
+# Replace 'path_to_results.json' with the actual path to your Trivy scan JSON file
+summarize_vulnerabilities('path_to_results.json')
+
